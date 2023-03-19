5d2ea51efb2a3f2956500bbe6fab3919d08094e0
Jonas Hermon-duc <jhermon-@student.42lausanne.ch>
2023-03-19 16:36:25 +0100

    cpp03 ex00 working

---
+++ cpp03/ex01/main.cpp
@@ -0,0 +1,17 @@
+#include "ClapTrap.hpp"
+
+int main( void ) {
+    ClapTrap    *ran = new ClapTrap;
+    ClapTrap    roro("Roro");
+
+    ran->beRepaired(2);
+    ran->attack("Roro");
+    ClapTrap    temp = *ran;
+    temp.takeDamage(10);
+    roro.attack("temp");
+    std::cout << "Deleting ran\n";
+    delete ran;
+    temp.takeDamage(10);
+
+    return 0;
+}
