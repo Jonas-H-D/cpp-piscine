35b79d2bd9de9996b873fe1b31b8fdb8f79a0c45
Jonas HD <jonas.hermonduc@gmail.com>
2023-05-01 11:53:46 +0200

    ex02 finished; needs last reading

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -12,8 +12,12 @@ class Array
 {
 public:
     // Constructors
-    Array() : _data(new T[0]), _size(0){};
-    Array(unsigned int n) : _data(new T[n]), _size(n){};
+    Array() : _data(new T[0]), _size(0){}
+    Array(unsigned int n) : _data(new T[n]), _size(n) {
+        for (unsigned int i = 0; i < n; i++) {
+            _data[i] = 0;
+        }
+    }
     Array(const Array<T>& other): _data(new T[other._size]), _size(other._size){
         for(int i = 0; i < (int)_size; i++){
             this->_data[i] = other._data[i];
@@ -24,21 +28,53 @@ public:
     ~Array() {delete[] _data; }
 
     // Assignment operator
-    Array<T>& operator=(const Array<T>& other);
+    Array<T>& operator=(const Array<T>& other){
+        if (this != &other){
+            delete[] _data;
+            _size = other._size;
+            _data = new T[_size];
+            for (unsigned int i = 0; i < _size; i++){
+                _data[i] = other._data[i];
+            }
+        }
+        return *this;
+    }
 
     // Subscript operator
-    T& operator[](unsigned int index);
+    T& operator[](unsigned int i) const {
+        if (i >= _size) {
+            throw OutOfRange();
+        }
+        return _data[i];
+    }
+
+    class OutOfRange : public std::exception {
+    public:
+        virtual const char* what() const throw() { return "Index is out of array range";}
+    };
 
     // Size function
-    unsigned int size() const;
-
-    // Printing functions
-    void printArray() const;
-    template <typename U>
-    void printTypeArray() const;
-
+    unsigned int size() const{
+        return _size;
+    }
 private:
+
     T* _data;
     unsigned int _size;
 };
+
+template < typename T >
+void printObjectFunction(Array<T> &tab, unsigned int size){
+    std::cout << "Array Printing" << std::endl;
+    for(int i=0; i < size; i++){
+        std::cout << tab[i] << " ";
+    }
+}
+
+template < typename T > void printTypeFunction(T& tab, unsigned int size)
+{
+    for (unsigned int i = 0; i < size; i++)
+        std::cout << tab[i] << " ";
+}
+
 #endif //CPP_PISCINE_ARRAY_HPP
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -1,9 +1,70 @@
-#include "Array.hpp"
 #include <iostream>
+#include "Array.hpp"
 
-int main()
+#define MAX_VAL 750
+int main(int, char**)
 {
-    int * a = new int();
-    std::cout << *a << std::endl;
+    // TEST 1 : Array Object and Array Typed
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    std::cout << "Test Array Generation" << std::endl
+              << "array[] numbers: ";
+    printObjectFunction(numbers, MAX_VAL);
+    std::cout << std::endl
+              << "array[] mirror: ";
+    printTypeFunction(mirror, MAX_VAL);
+    std::cout << std::endl;
+    // TEST 2 : Copy OPERATOR = and Constructor with Object copy
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+        std::cout << "Test Array Generation" << std::endl
+                  << "array[] tmp: ";
+        printObjectFunction(tmp, MAX_VAL);
+        std::cout << std::endl
+                  << "array[] test: ";
+        printObjectFunction(test, MAX_VAL);
+        std::cout << std::endl;
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        std::cout << "Trying to access tab i - 1 : should error" << std::endl;
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        std::cout << "Trying to access array[MAX_VALUE]. Should error" << std::endl;
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//
     return 0;
 }
