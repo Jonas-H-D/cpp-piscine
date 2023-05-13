ac120c970c861e189c094d109fb594c7ab1e1619
Jonas HD <jonas.hermonduc@gmail.com>
2023-05-13 11:38:40 +0200

    cpp08 finished

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,11 +6,25 @@
 #include <iostream>
 #include <unistd.h>
 #include <list>
+#include <vector>
 
 template<typename T, typename Container = std::deque<T> >
 class MutantStack : public std::stack<T, Container>
 {
 public:
+
+    MutantStack() {}
+    MutantStack(const MutantStack &rhs) {
+        this->c = rhs.c;
+    }
+    MutantStack& operator=(const MutantStack &rhs) {
+        if (this != &rhs) {
+            this->c = rhs.c;
+        }
+        return *this;
+    }
+    ~MutantStack() {}
+
     typedef typename Container::iterator iterator;
     typedef typename Container::const_iterator const_iterator;
 
@@ -22,11 +36,11 @@ public:
         return this->c.end();
     }
 
-    const_iterator begin() const {
+    const_iterator cbegin() const {
         return this->c.begin();
     }
 
-    const_iterator end() const {
+    const_iterator cend() const {
         return this->c.end();
     }
 };
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -1,5 +1,4 @@
 #include "MutantStack.hpp"
-
 int main()
 {
  std::cout << "-------This IS THE MUTANT STACK" << std::endl;
@@ -24,32 +23,92 @@ int main()
         ++it;
     }
     std::stack<int> s(mstack);
-    return 0;
-}
-/*
-int main()
-{
-    std::cout << "-------This IS THE LIST" << std::endl;
-    std::list<int> mstack;
-    mstack.push_front(5);
-    mstack.push_front(17);
-    std::cout << mstack.front() << std::endl;
-    mstack.pop_front();
-    std::cout << mstack.size() << std::endl;
-    mstack.push_front(3);
-    mstack.push_front(5);
-    mstack.push_front(737);
+
+    std::cout << "-------This IS THE ** CONST ** MUTANT STACK" << std::endl;
+    MutantStack<const int> cstack;
+    cstack.push(5);
+    cstack.push(17);
+    std::cout << cstack.top() << std::endl;
+    cstack.pop();
+    std::cout << cstack.size() << std::endl;
+    cstack.push(3);
+    cstack.push(5);
+    cstack.push(737);
 //[...]
-    mstack.push_front(0);
-    std::list<int>::iterator it = mstack.begin();
-    std::list<int>::iterator ite = mstack.end();
+    cstack.push(0);
+    MutantStack<const int>::const_iterator cit = cstack.cbegin();
+    MutantStack<const int>::const_iterator cite = cstack.cend();
+    ++cit;
+    --cit;
+    while (cit != cite)
+    {
+        std::cout << *cit << std::endl;
+        ++cit;
+    }
+    std::stack<const int> c(cstack);
+
+    std::cout << "-------This IS THE LIST" << std::endl;
+    std::list<int> lstack;
+    lstack.push_back(5);
+    lstack.push_back(17);
+    std::cout << lstack.back() << std::endl;
+    lstack.pop_back();
+    std::cout << lstack.size() << std::endl;
+    lstack.push_back(3);
+    lstack.push_back(5);
+    lstack.push_back(737);
+//[...]
+    lstack.push_back(0);
+    std::list<int>::iterator lit = lstack.begin();
+    std::list<int>::iterator lite = lstack.end();
     ++it;
     --it;
-    while (it != ite)
+    while (lit != lite)
     {
-        std::cout << *it << std::endl;
-        ++it;
+        std::cout << *lit << std::endl;
+        ++lit;
     }
-    std::list<int> s(mstack);
+    std::list<int> l(lstack);
+
+    std::cout << "-------This IS THE VECTOR" << std::endl;
+    std::vector<int> vstack;
+    vstack.push_back(5);
+    vstack.push_back(17);
+    std::cout << vstack.back() << std::endl;
+    vstack.pop_back();
+    std::cout << vstack.size() << std::endl;
+    vstack.push_back(3);
+    vstack.push_back(5);
+    vstack.push_back(737);
+    //[...]
+    vstack.push_back(0);
+    std::vector<int>::iterator vit = vstack.begin();
+    std::vector<int>::iterator vite = vstack.end();
+    ++vit;
+    --vit;
+    while (vit != vite) {
+        std::cout << *vit << std::endl;
+        ++vit;
+    }
+    std::vector<int> v(vstack);
+
+    std::cout << "-------STACK OF STRINGS" << std::endl;
+    MutantStack<std::string> cityStack;
+    cityStack.push("London");
+    cityStack.push("Paris");
+    cityStack.push("New York");
+    cityStack.pop();
+    cityStack.push("Tokyo");
+    cityStack.push("Sydney");
+    std::cout << "Cities in the stack:" << std::endl;
+    MutantStack<std::string>::iterator it2 = cityStack.begin();
+    MutantStack<std::string>::iterator ite2 = cityStack.end();
+    ++it2;
+    --it2;
+    while (it2 != ite2) {
+        std::cout << *it2 << std::endl;
+        ++it2;
+    }
+    MutantStack<std::string> city(cityStack);
     return 0;
-}*/
+}
