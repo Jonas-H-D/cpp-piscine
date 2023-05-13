b3e9bcad5427ef48dfcf3c8570dc25d4d83cee18
Jonas HD <jonas.hermonduc@gmail.com>
2023-05-13 14:34:23 +0200

    ready for evaluation cpp08

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -12,77 +12,72 @@ std::vector<int> generateRandomNumbers(unsigned int count) {
 }
 
 int main(void){
+    std::cout << "-----GENERATING BIG SPAN " << std::endl;
     try {
-        std::cout << "-----GENERATING BIG SPAN " << std::endl;
-        try {
-            Span mySpan(10000);
-            std::vector<int> numbers = generateRandomNumbers(10000);
-            mySpan.addNumbers(numbers.begin(), numbers.end());
+        Span mySpan(100000);
+        std::vector<int> numbers = generateRandomNumbers(100000);
+        mySpan.addNumbers(numbers.begin(), numbers.end());
 
-            int shortest = mySpan.shortestSpan();
-            int longest = mySpan.longestSpan();
-            std::cout << "Shortest span = " << shortest << std::endl;
-            std::cout << "Longest span = " << longest << std::endl;
-        } catch (const std::exception& e) {
-            std::cerr << e.what() << std::endl;
-        }
+        int shortest = mySpan.shortestSpan();
+        int longest = mySpan.longestSpan();
+        std::cout << "Shortest span = " << shortest << std::endl;
+        std::cout << "Longest span = " << longest << std::endl;
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+    usleep(1000000);
+    std::cout << "-----GENERATING SMALL SPAN " << std::endl;
+    try {
+        Span sp = Span(5);
+        sp.addNumber(6);
+        sp.addNumber(3);
+        sp.addNumber(17);
+        sp.addNumber(9);
+        sp.addNumber(11);
+        std::cout << "Shortest span = " << sp.shortestSpan() << std::endl;
+        std::cout << "Longest span = " << sp.longestSpan() << std::endl;
+    } catch(const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+    std::cout << "-----CREATING SPANNY " << std::endl;
+    try {
+        Span *spanny = new Span(-10000);
         usleep(1000000);
-        std::cout << "-----GENERATING SMALL SPAN " << std::endl;
-        try {
-            Span sp = Span(5);
-            sp.addNumber(6);
-            sp.addNumber(3);
-            sp.addNumber(17);
-            sp.addNumber(9);
-            sp.addNumber(11);
-            std::cout << "Shortest span = " << sp.shortestSpan() << std::endl;
-            std::cout << "Longest span = " << sp.longestSpan() << std::endl;
-        } catch(const std::exception& e) {
-            std::cerr << e.what() << std::endl;
-        }
-            std::cout << "-----CREATING SPANNY " << std::endl;
-        try {
-            Span *spanny = new Span(10000);
-            usleep(1000000);
 
-            std::vector<int> newNumbers = generateRandomNumbers(10000);
-            spanny->addNumbers(newNumbers.begin(), newNumbers.end());
-            int shorty = spanny->shortestSpan();
-            int longy = spanny->longestSpan();
-            std::cout << "Shortest spanny = " << shorty << std::endl;
-            std::cout << "Longest spanny = " << longy<< std::endl;
-            delete spanny;
-        } catch(const std::exception& e) {
-            std::cerr << e.what() << std::endl;
-        }
-        std::cout << "-----REACHING SPAN CAPACITY " << std::endl;
-        try {
-            Span sp = Span(5);
-            sp.addNumber(6);
-            sp.addNumber(3);
-            sp.addNumber(17);
-            sp.addNumber(9);
-            sp.addNumber(11);
-            sp.addNumber(100);
-        } catch(const std::exception& e) {
-            std::cerr << e.what() << std::endl;
-        }
-        std::cout << "-----CREATING SPANN COPY " << std::endl;
-        try {
-            Span *spanny = new Span(10000);
-            usleep(100000);
-            std::vector<int> newNumbers = generateRandomNumbers(10000);
-            spanny->addNumbers(newNumbers.begin(), newNumbers.end());
-            Span spanCop = *spanny;
-            int shortcop = spanCop.shortestSpan();
-            int longcop= spanCop.longestSpan();
-            std::cout << "Shortest spanny = " << shortcop << std::endl;
-            std::cout << "Longest spanny = " << longcop<< std::endl;
-            delete spanny;
-        } catch(const std::exception& e) {
-            std::cerr << e.what() << std::endl;
-        }
-
+        std::vector<int> newNumbers = generateRandomNumbers(10000);
+        spanny->addNumbers(newNumbers.begin(), newNumbers.end());
+        int shorty = spanny->shortestSpan();
+        int longy = spanny->longestSpan();
+        std::cout << "Shortest spanny = " << shorty << std::endl;
+        std::cout << "Longest spanny = " << longy<< std::endl;
+        delete spanny;
+    } catch(const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+    std::cout << "-----REACHING SPAN CAPACITY " << std::endl;
+    try {
+        Span sp = Span(5);
+        sp.addNumber(6);
+        sp.addNumber(3);
+        sp.addNumber(17);
+        sp.addNumber(9);
+        sp.addNumber(11);
+        sp.addNumber(100);
+    } catch(const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+    std::cout << "-----CREATING SPANN COPY " << std::endl;
+    try {
+        Span *spanny = new Span(10000);
+        usleep(100000);
+        std::vector<int> newNumbers = generateRandomNumbers(10000);
+        spanny->addNumbers(newNumbers.begin(), newNumbers.end());
+        Span spanCop = *spanny;
+        int shortcop = spanCop.shortestSpan();
+        int longcop= spanCop.longestSpan();
+        std::cout << "Shortest spanny = " << shortcop << std::endl;
+        std::cout << "Longest spanny = " << longcop<< std::endl;
+        delete spanny;
     } catch(const std::exception& e) {
         std::cerr << e.what() << std::endl;
     }
